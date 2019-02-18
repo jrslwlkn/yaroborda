28ddeec0ccaaf8d1f173a7c3f0e6b89661dfe3b8
Yarek <jrslwlkn@gmail.com>
2019-02-17 22:53:38 -0600

    fixed post render bug

--- client/src/components/post-text/PostText.js
+++ client/src/components/post-text/PostText.js
@@ -3,11 +3,12 @@ import Md from 'react-markdown';
 
 import Replies from './replies';
 import ShowFullText from './ShowFullText';
+import './post-text.css';
 
 export default function PostText({ text }) {
     return (
-        <div className="v-top lh-little mv0 tl">
-            <Md source={text} renderers={{ paragraph: 'span' }} />
+        <div className="v-top lh-little mv0 tl post-text">
+            <Md source={text} />
             <ShowFullText />
             <Replies />
         </div>
