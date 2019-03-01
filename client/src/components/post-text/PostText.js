d3e371b886023c306b04836f8ccf3940cb99689e
Yarek <jrslwlkn@gmail.com>
2019-02-28 23:59:17 -0600

    re-making show full text

--- client/src/components/post-text/PostText.js
+++ client/src/components/post-text/PostText.js
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { Component } from 'react';
 
+// to be removed
 import ShowMoreText from 'react-show-more-text';
 import Md from 'react-markdown/with-html';
 import mdToHtml from 'marked';
@@ -8,23 +9,54 @@ import Replies from './replies';
 import ShowFullText from './ShowFullText';
 import './post-text.css';
 
-export default function PostText({ text }) {
-    const otherFunc = () => console.log('other f');
+class PostText extends Component {
+    state = {
+        isRevealed: false,
+        isShort: true,
+        text: ''
+    }
 
-    window.someFunc = (id) => {
-        otherFunc();
-        console.log(id);
-    };
+    componentDidMount = () => {
+        const { text } = this.props;
 
-    const updatedMdText = text.replace(/(->([0-9]+))/gm, '[$1](javascript:someFunc($2))');
+        // to be changed
+        if (text.length > 100) {
+            this.setState({ isShort: false });
+        }
+    }
 
-    return (
-        <div className="v-top lh-little mv0 tl post-text">
-            <ShowMoreText>
-                <Md source={mdToHtml(updatedMdText)} escapeHtml={false} />
-            </ShowMoreText>
-            {/* <ShowFullText />
+    toggleFullText = () => {
+        this.setState(state => ({ isRevealed: !state.isRevealed }));
+    }
+
+
+    otherFunc = () => console.log('other f');
+
+    someFunc = () => {
+        window.someFunc = (id) => {
+            this.otherFunc();
+            console.log(id);
+        };
+    }
+
+    render() {
+        const { text } = this.props;
+        const updatedMdText = text.replace(/(->([0-9]+))/gm, '[$1](javascript:someFunc($2))');
+
+        return (
+            <div className="v-top lh-little mv0 tl post-text">
+                <ShowMoreText
+                    lines={8}
+                    more="Show full post"
+                    less="Collapse it"
+                >
+                    <Md source={mdToHtml(updatedMdText)} escapeHtml={false} />
+                </ShowMoreText>
+                {/* <ShowFullText />
             <Replies /> */}
-        </div>
-    );
+            </div>
+        );
+    }
 }
+
+export default PostText;
